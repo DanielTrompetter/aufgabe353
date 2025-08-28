e10f1d0b1b4cf1f98b0f8598c324becafe0125cb
Daniel Trompetter <danieltrompetter@gmail.com>
2025-08-28 11:15:24 +0200

    added aufgabe 3

--- bin/aufgabe353.dart
+++ bin/aufgabe353.dart
@@ -12,6 +12,14 @@ void main()
   print('Anzahl Buchstaben in "$text1" ist ${getLengthOfText(text1)}');
   text1 = "Diese Aufgabe ist am lanweiligsten!";
   print('Anzahl Buchstaben in "$text1" ist ${getLengthOfText(text1)}');
+
+  // aufgabe 3
+  String text2 = 'Finde die Vokale!';
+  print('Anzahl Vokale in "$text2" ist ${getNumberOfVocals(text2)}');
+  text2 = "Und gefunden?!";
+  print('Anzahl Vokale in "$text2" ist ${getNumberOfVocals(text2)}');
+  text2 = "Nun los sag an!";
+  print('Anzahl Vokale in "$text2" ist ${getNumberOfVocals(text2)}');
 }
 
 // funktion fuer aufgabe 1
@@ -25,4 +33,19 @@ int add3numbers(int a, int b, int c)
 int getLengthOfText(String text)
 {
   return text.length;
+}
+
+// funktion fuer aufgabe 3
+int getNumberOfVocals(String text)
+{
+  List<String> vokals = ['a', 'e', 'i', 'o', 'u', 'ä', 'ö', 'ü'];
+  int numVokals = 0;
+  for(String vokal in vokals)
+  {
+    for(int i = 0; i < text.length; i++)
+    {
+      text[i] == vokal? numVokals++:();
+    }
+  }
+  return numVokals;
 }
